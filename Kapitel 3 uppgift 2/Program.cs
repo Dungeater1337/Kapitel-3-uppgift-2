77e1f329aa81be27d39ecf9e0a713b8c53ee0b13
Alka04 <alka04006@utb.vaxjo.se>
2022-09-30 10:58:54 +0200

    Add project files.

---
+++ Kapitel 3 uppgift 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System; 
+
+namespace Uppgift3_14
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet?");
+            string svar = Console.ReadLine().ToLower();
+            
+
+            if (svar == "j")
+            {
+                Console.WriteLine("Okej hur gammal är du?"); 
+                int ålder = int.Parse(Console.ReadLine());
+
+                if (ålder <= 22 && ålder >= 18)
+                {
+                    Console.WriteLine("Vi skulle vilja anställa dig.");
+                }
+
+                else
+                {
+                    Console.WriteLine("vi letar tyvärr efter annan personal just nu");
+                }
+            }
+                
+                else if (svar == "n")
+            {
+                Console.WriteLine("Dra dååååå"); 
+            }
+                
+            
+        }
+    }
+}
